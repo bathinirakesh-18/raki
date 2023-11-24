26804b352a552ee478365112aea26d022a3fe6ab
bathinirakesh-18 <151864367+bathinirakesh-18@users.noreply.github.com>
2023-11-24 16:42:16 +0000

    Changed the content which has to be return

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def incredible():
-  return 'unbelieveable man'
+  return 'unbelieveable, its so surprising'
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0',debug=True)
